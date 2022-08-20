d1952131901a0aaa65892b8239e5f4183d9602af
Steve Beville <steve_beville@hotmail.com>
2022-08-20 21:16:56 +0100

    add camera movement w/ arrow keys

--- lib/game.dart
+++ lib/game.dart
@@ -1,13 +1,19 @@
 import 'dart:math';
 
 import 'package:flame/game.dart';
+import 'package:flame/input.dart';
 import 'package:flamejam/background.dart';
 import 'package:flamejam/model/map_data.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class MyGame extends FlameGame {
+class MyGame extends FlameGame with KeyboardEvents {
   MapData mapData = MapData(128, 128);
+  Vector2 cameraVector = Vector2(600, 600);
+  int cameraMoveValue = 15;
+
   @override
-  Future<void>? onLoad() {
+  Future<void> onLoad() async {
     super.onLoad();
     double tileSize = 16.0;
     add(Background(
@@ -15,13 +21,62 @@ class MyGame extends FlameGame {
       tileSize: tileSize,
       data: mapData,
     ));
-    return null;
+    camera.followVector2(cameraVector);
+    camera.worldBounds = Rect.fromLTWH(
+      0,
+      0,
+      mapData.width * tileSize,
+      mapData.height * tileSize,
+    );
   }
 
   @override
   void update(double dt) {
     mapData.setTerritory(Random().nextInt(mapData.width),
         Random().nextInt(mapData.height), Random().nextInt(2) + 1);
+    camera.update(dt);
     super.update(dt);
+  }
+
+  @override
+  KeyEventResult onKeyEvent(
+    RawKeyEvent event,
+    Set<LogicalKeyboardKey> keysPressed,
+  ) {
+    if (keysPressed.contains(LogicalKeyboardKey.arrowDown)) {
+      cameraVector.setValues(
+        cameraVector.x,
+        min(cameraVector.y + cameraMoveValue,
+            camera.worldBounds!.bottom - camera.viewport.canvasSize!.y / 2),
+      );
+      return KeyEventResult.handled;
+    }
+
+    if (keysPressed.contains(LogicalKeyboardKey.arrowUp)) {
+      cameraVector.setValues(
+        cameraVector.x,
+        max(cameraVector.y - cameraMoveValue,
+            camera.worldBounds!.top + camera.viewport.canvasSize!.y / 2),
+      );
+      return KeyEventResult.handled;
+    }
+
+    if (keysPressed.contains(LogicalKeyboardKey.arrowLeft)) {
+      cameraVector.setValues(
+          max(cameraVector.x - cameraMoveValue,
+              camera.worldBounds!.left + camera.viewport.canvasSize!.x / 2),
+          cameraVector.y);
+      return KeyEventResult.handled;
+    }
+
+    if (keysPressed.contains(LogicalKeyboardKey.arrowRight)) {
+      cameraVector.setValues(
+          min(cameraVector.x + cameraMoveValue,
+              camera.worldBounds!.right - camera.viewport.canvasSize!.x / 2),
+          cameraVector.y);
+      return KeyEventResult.handled;
+    }
+
+    return KeyEventResult.ignored;
   }
 }
